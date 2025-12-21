51585eb1cc2427c9fd9cd18738a1629f236cfc08
Abhishek yadav <studyandrotech@gmail.com>
2025-12-21 23:56:19 +0530

    useContext

--- REACTJS_PRACTICE_SEST/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { Fakedata } from "./pages/fakedata"
-export const App = ()=>{
-    return (
-        <div className="bg-black h-screen w-screen text-white">
-            {/* <h1 className="text-red-400"></h1> */}
-            <Fakedata />
-        </div>
-    )
-}
